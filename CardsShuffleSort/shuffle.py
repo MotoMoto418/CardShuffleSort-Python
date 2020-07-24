193798e91f7637cc131c9b5c21474fd645f4b263
SarangaKulkarni <66240313+SarangaKulkarni@users.noreply.github.com>
2020-07-24 13:05:59 +0530

    Update shuffle.py

--- CardsShuffleSort/shuffle.py
+++ CardsShuffleSort/shuffle.py
@@ -2,15 +2,18 @@ from random import *
 from cards import *
 
 def Shuffle(lst):
+    i = 0
+    emp = []
 
-    for ele in lst:
+    while i < 52:
         num1 = randint(0, 51)
         num2 = randint(0, 51)
 
-        if num1 != num2:
+        if num1 != num2 and (num1, num2) not in emp:
             temp = lst[num1]
             lst[num1] = lst[num2]
             lst[num2] = temp
+            i += 1
+            emp.append((num1, num2))
 
     return ''
-
